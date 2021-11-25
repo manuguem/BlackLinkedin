09bf96b1ccb8b206b6ff1d11f247db8dbc035f75
Manuel Guerra <manu.guerra2010@gmail.com>
2021-11-25 20:07:19 -0300

    Update BlackLinkedin

--- showhide.js
+++ showhide.js
@@ -1,20 +1,19 @@
-function Show () {
-    document.getElementById("comment").style.display = "block";
+function Show() {
+  document.getElementById("comment").style.display = "block";
 }
 
-function Hide () {
-    document.getElementById("comment").style.display = "none";
+function Hide() {
+  document.getElementById("comment").style.display = "none";
 }
 
-function Show_Hide () {
-    const comment = document.getElementById("comment");
+function Show_Hide() {
+  const comment = document.getElementById("comment");
 
-    if(comment.style.display == "none") {
-        Show();
-        document.getElementById("button").value = "Ocultar"
-    }
-    else {
-        Hide();
-        document.getElementById("button").value = "Mostrar más comentarios"
-    }
+  if (comment.style.display == "none") {
+    Show();
+    document.getElementById("button").value = "Ocultar";
+  } else {
+    Hide();
+    document.getElementById("button").value = "Mostrar más comentarios";
+  }
 }
